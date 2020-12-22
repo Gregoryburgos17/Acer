1654c7f09b4d3a5488b855c313568654884d8a7d
Gregoryburgos17 <61199931+Gregoryburgos17@users.noreply.github.com>
2020-12-22 16:50:42 -0500

    Add files via upload

---
+++ VEHICULO/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VEHICULO
+{
+    class Class1
+    {
+    }
+}
---
+++ VEHICULO/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace VEHICULO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int u = 0;
+            Console.WriteLine("hola bienvenido a mis proyecto");
+            u = int.Parse(Console.ReadLine());
+              
+
+
+
+
+
+        }
+    }
+    class circulo
+    {
+        
+
+
+
+    }
+    class verde
+    {
+           
+
+
+
+    }  
+      class Rojo { }
+    
+    
+
+
+
+
+    
+        
+
+
+
+
+}   
