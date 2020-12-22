f321bf6551f91724abc4dbdc65f268092365289b
Gregoryburgos17 <61199931+Gregoryburgos17@users.noreply.github.com>
2020-12-22 17:01:51 -0500

    Add files via upload

---
+++ tarea2capas/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace tarea2capas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btncrear = new System.Windows.Forms.Button();
+            this.comboFiltro = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textCE = new System.Windows.Forms.TextBox();
+            this.textnombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboman = new System.Windows.Forms.ComboBox();
+            this.textApt = new System.Windows.Forms.TextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.Yellow;
+            this.btnEditar.Location = new System.Drawing.Point(423, 243);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(61, 42);
+            this.btnEditar.TabIndex = 0;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.Red;
+            this.btnBorrar.Location = new System.Drawing.Point(423, 291);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(61, 40);
+            this.btnBorrar.TabIndex = 1;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btncrear
+            // 
+            this.btncrear.BackColor = System.Drawing.Color.Lime;
+            this.btncrear.Location = new System.Drawing.Point(423, 195);
+            this.btncrear.Name = "btncrear";
+            this.btncrear.Size = new System.Drawing.Size(61, 42);
+            this.btncrear.TabIndex = 2;
+            this.btncrear.Text = "Registro";
+            this.btncrear.UseVisualStyleBackColor = false;
+            this.btncrear.Click += new System.EventHandler(this.btncrear_Click);
+            // 
+            // comboFiltro
+            // 
+            this.comboFiltro.FormattingEnabled = true;
+            this.comboFiltro.Location = new System.Drawing.Point(69, 152);
+            this.comboFiltro.Name = "comboFiltro";
+            this.comboFiltro.Size = new System.Drawing.Size(173, 21);
+            this.comboFiltro.TabIndex = 4;
+            this.comboFiltro.SelectedIndexChanged += new System.EventHandler(this.comboFiltro_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(391, 134);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textCE
+            // 
+            this.textCE.Location = new System.Drawing.Point(546, 57);
+            this.textCE.Name = "textCE";
+            this.textCE.Size = new System.Drawing.Size(100, 20);
+            this.textCE.TabIndex = 6;
+            // 
+            // textnombre
+            // 
+            this.textnombre.Location = new System.Drawing.Point(546, 83);
+            this.textnombre.Name = "textnombre";
+            this.textnombre.Size = new System.Drawing.Size(100, 20);
+            this.textnombre.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(475, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "cedula ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(475, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(475, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "manzana";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(475, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "edificio";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(475, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "apartamento";
+            // 
+            // comboman
+            // 
+            this.comboman.FormattingEnabled = true;
+            this.comboman.Location = new System.Drawing.Point(531, 146);
+            this.comboman.Name = "comboman";
+            this.comboman.Size = new System.Drawing.Size(121, 21);
+            this.comboman.TabIndex = 11;
+            // 
+            // textApt
+            // 
+            this.textApt.Location = new System.Drawing.Point(546, 179);
+            this.textApt.Name = "textApt";
+            this.textApt.Size = new System.Drawing.Size(100, 20);
+            this.textApt.TabIndex = 6;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.BackColor = System.Drawing.Color.Turquoise;
+            this.btnbuscar.Location = new System.Drawing.Point(12, 141);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(51, 40);
+            this.btnbuscar.TabIndex = 3;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = false;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(546, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(539, 161);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(531, 116);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 352);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboman);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textApt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textnombre);
+            this.Controls.Add(this.textCE);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboFiltro);
+            this.Controls.Add(this.btnbuscar);
+            this.Controls.Add(this.btncrear);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnEditar);
+            this.Name = "Form1";
+            this.Text = "CONDOMINIOS G.B";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btncrear;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.ComboBox comboFiltro;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textCE;
+        private System.Windows.Forms.TextBox textnombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboman;
+        private System.Windows.Forms.TextBox textApt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ tarea2capas/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassNegocio;
+using ClassEntidades;
+
+namespace tarea2capas
+{
+    public partial class Form1 : Form
+    {
+        ClassEn estudi = new ClassEn();
+        Classnegocio ejecutor = new Classnegocio ();
+       // private string opcionFiltro;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = ejecutor.Read();
+        }
+
+        private void btncrear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                estudi.cedula = int.Parse(textCE.Text);
+                estudi.nombre = textnombre.Text;
+                estudi.edificio = comboBox2.SelectedValue.ToString(); 
+                estudi.manzana = comboman.SelectedValue.ToString();
+                estudi.apartamento = int.Parse(textApt.Text);
+
+                ejecutor.Create(estudi);
+                MessageBox.Show("Valores ingresados!!");
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ejecutor.Read();
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                estudi.cedula = int.Parse(textCE.Text);
+                estudi.nombre = textnombre.Text;
+                estudi.edificio = comboBox2.SelectedValue.ToString();
+                estudi.manzana = comboman.SelectedValue.ToString();
+                estudi.apartamento = int.Parse(textCE.Text);
+
+                ejecutor.modificar(estudi);
+                MessageBox.Show("Valores ingresados!!");
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            //dataGridView1.DataSource = ejecutor.Read();
+            
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                estudi.cedula = int.Parse(textCE.Text);
+                ejecutor.Delete(estudi);
+                MessageBox.Show("Valores ingresados!!");
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarMAN();
+            CargarEDI();
+            limpiar();
+        }
+        public void limpiar()
+        {
+            textCE.Clear();
+            textnombre.Clear();
+            textApt.Clear();
+            comboBox2.Text = "";
+            comboman.Text="";
+
+
+        }
+
+        public void CargarMAN()
+        {
+            comboman.DataSource = ejecutor.CargarManza();
+            comboman.ValueMember = "Id_Manza";
+            comboman.DisplayMember = "manzana";
+            comboman.SelectedIndex = -1;
+        }
+        public void CargarEDI()
+        {
+            comboBox2.DataSource = ejecutor.CargarEdif();
+            comboBox2.ValueMember = "Id_EDI";
+            comboBox2.DisplayMember = "edificio";
+            comboBox2.SelectedIndex = -1;
+        }
+
+        private void comboFiltro_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textCE.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textnombre.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            comboBox2.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            comboman.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textApt.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+    }
+}
