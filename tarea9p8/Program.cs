ec852962327a1fd8c24260f1385d84f1e8b22d4a
Gregoryburgos17 <61199931+Gregoryburgos17@users.noreply.github.com>
2020-12-22 16:55:30 -0500

    Add files via upload

---
+++ tarea9p8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea9p8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            int contador=0, num=0;
+            for (int i=0; i<numeros.Length; i++)
+            {
+                Console.WriteLine("Digite numero " + i);
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+
+            foreach (int nums in numeros)
+            {
+                num=nums;
+                while ( num> 10)
+                {
+                   num  =num  / 10;
+
+                }
+                if ( num== 4)
+                {
+                    contador++;
+                }
+            }
+            Console.Write("EL NUMERO 4 SE REPITE{0}", contador);
+        }
+    }
+}
