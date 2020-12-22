2dd401530074ef9ade039a0a76e970d4845ffb92
Gregoryburgos17 <61199931+Gregoryburgos17@users.noreply.github.com>
2020-12-22 18:05:56 -0500

    Add files via upload

---
+++ fundamento 5tarea 5p2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace fundamento_5tarea_5p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double pesos, dolar, colombiano;
+            Console.WriteLine("programa que convierte el peso dolar y peso colombiano\n");
+            Console.WriteLine("digite a cantidad de peso a convertir");
+            pesos = Convert.ToDouble(Console.ReadLine());
+            dolar = (pesos * 0.019);
+            colombiano = (pesos * 65.16);
+            Console.WriteLine("el monto de dolares que tiene es {0}", dolar);
+            Console.WriteLine("el monto de pesos colombiano que tiene es {0}", colombiano);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ fundamento proyecto final12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fundamento_proyecto_final12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
