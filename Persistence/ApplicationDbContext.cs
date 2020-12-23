d1a8a63784215ae7abfd317b47ee59ec2329fd5b
Gregoryburgos17 <61199931+Gregoryburgos17@users.noreply.github.com>
2020-12-22 20:59:19 -0500

    Add files via upload

---
+++ Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")//ir al web config a para hacer la conexion a sqlite
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
