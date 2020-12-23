61ab1efd547d4164bbd7cbeceb097970e1b01f4c
Gregoryburgos17 <61199931+Gregoryburgos17@users.noreply.github.com>
2020-12-22 21:39:18 -0500

    Add files via upload

---
+++ prueda de escritorio/Program.cs
@@ -0,0 +1,522 @@
+﻿using System;
+using System.Linq;
+namespace prueda_de_escritorio
+{
+    class Program
+    {
+        static string contacto;
+        static string eventos;
+        static string utilitarios;
+        static int respardo;
+        
+        static string[] nombres = new string[500];
+        static string[] telefono = new string[500];
+        static string[] email = new string[500];
+        static string[] direccion = new string[500];
+        static string[] actividad = new string[500];
+        static string[] fecha = new string[500];
+        static string[] hora = new string[500];
+        static string[] lugar = new string[500];
+        static int x = -1;
+        static void Main()
+        {
+           
+
+            
+            
+            
+            
+                Console.WriteLine("Introduzca el valor del menú en el que desea ingresar: " +
+                    "" + "\n" + "[1] Contactos" + "\n" + "[2] Eventos" + "\n" + "[3] Utilitarios" + "\n" + "[4]salir");
+                string cadenaEvaluar = Console.ReadLine();
+                bool esLetra = cadenaEvaluar.All(char.IsLetter);
+                bool esNumero= cadenaEvaluar.All(char.IsNumber);
+                bool esNumer = false;
+                int hum = 0;
+                try
+                {
+                    hum = Convert.ToInt32(cadenaEvaluar);
+                }
+                catch (Exception)
+                {
+                    esNumer = true;
+
+                }
+                finally
+                {
+                    if (esNumer == true || hum > 4)
+                    {
+                        Console.Clear();
+                        Main();
+
+                    }
+                    else
+                    {
+                        switch (hum)
+                        {
+                            case 1:
+                                Contacto();
+
+                                break;
+
+                            case 2:
+                                Eventos();
+                                break;
+
+                            case 3:
+                                Utilitarios();
+                                break;
+
+                            case 4: break;
+                            /*default:
+                                Console.Clear();
+                                Console.WriteLine("error 404");
+                                Console.ReadLine();
+                                Console.Clear();*/
+                                
+
+                        }
+
+                        Console.ReadKey();
+                    }
+                }
+
+
+              
+        }
+        static void Contacto()
+        {
+            int opciones = 0;
+            
+            
+                Console.Clear();
+                Console.WriteLine("Introduzca el valor del menú en el que desea ingresar: " +
+              "" + "\n" + "[1] Agregar" + "\n" + "[2] Actualizar" + "\n" + "[3] Borrar" + "\n" + "[4] Buscar" + "\n" + "[5]atras");
+                string cadenaEvaluar = Console.ReadLine();
+                bool esLetra = cadenaEvaluar.All(char.IsLetter);
+                bool esNumero = cadenaEvaluar.All(char.IsNumber);
+                bool esNumer = false;
+               
+                try
+                {
+                    opciones = Convert.ToInt32(cadenaEvaluar);
+                }
+                catch (Exception)
+                {
+                    esNumer = true;
+
+                }
+                finally
+                {
+                    if (esNumer == true || opciones > 5)
+                    {
+                        Console.Clear();
+                        Contacto();
+
+                    }
+                    else
+                    {
+                    switch (opciones)
+                    {
+                        case 1:
+
+                            x++;
+                            Console.Clear();
+                            Console.Write("Introduzca el nombre: ");
+                            nombres[x] = Console.ReadLine();
+                            Console.Write("Introduzca el telefono: ");
+                            telefono[x] = Console.ReadLine();
+                            Console.Write("Introduzca el email: ");
+                            email[x] = Console.ReadLine();
+                            Console.Write("Introduzca la dirección: ");
+                            direccion[x] = Console.ReadLine();
+
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("CONTACTO DE NOMBRE");
+                            string busqueda2 = Console.ReadLine();
+
+                            Console.Clear();
+
+                            for (int i = 0; i < nombres.Length; i++)
+                            {
+                                if (nombres[i] == busqueda2 || email[i] == busqueda2)
+                                {
+                                    Console.WriteLine("ACTUALIZA CONTACTO" + "\b\n" + nombres[i]);
+                                    nombres[i] = Convert.ToString(Console.ReadLine());
+                                    Console.WriteLine("NUEVO CORREO" + "\b\n" + email[i]);
+                                    email[i] = Convert.ToString(Console.ReadLine());
+                                    Console.WriteLine("NUEVO NUMERO" + "\b\n" + telefono[i]);
+                                    telefono[i] = Convert.ToString(Console.ReadLine());
+                                    Console.WriteLine("NUEVA DIRECION" + "\b\n" + direccion[i]);
+                                    direccion[i] = Convert.ToString(Console.ReadLine());
+                                }
+                            }
+                            Console.ReadKey();
+                            break;
+
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("CONTACTO NOMBRE O CORREO");
+                            string busqueda3 = Console.ReadLine();
+
+                            Console.Clear();
+
+                            for (int i = 0; i < nombres.Length; i++)
+                            {
+                                if (nombres[i] == busqueda3)
+                                {
+                                    nombres[i] = null;
+                                }
+                            }
+                            Console.ReadKey();
+
+                            break;
+
+                        case 4:
+                            Console.Clear();
+                            Console.WriteLine("CONTACTO NOMBRE O CORREO");
+                            string busqueda = Console.ReadLine();
+
+                            Console.Clear();
+
+                            for (int i = 0; i < nombres.Length; i++)
+                            {
+                                if (nombres[i] == busqueda)
+                                {
+                                    Console.WriteLine("contacto " + i + "\b\ncontacto encontrado" + "\b\n" + nombres[i]);
+                                    Console.WriteLine("correo" + "\b\n" + email[i]);
+                                    Console.WriteLine("numero" + "\b\n" + telefono[i]);
+                                    Console.WriteLine("lugar" + "\b\n" + direccion[i]);
+                                }
+                            }
+                            Console.ReadKey();
+                            break;
+
+                        case 5:
+                            Console.Clear();
+                            Main();
+                            break;
+                    }
+
+                    Console.ReadKey();
+                    }
+                }
+
+                
+
+               
+
+            
+        }
+        static void Respardo()
+        {
+
+
+            for (int x = 0; x < nombres.Length; x++)
+            {
+                Console.Clear();
+                Console.Write("Introduzca el nombre: ");
+                nombres[x] = Console.ReadLine();
+                Console.Write("Introduzca el telefono: ");
+                telefono[x] = Console.ReadLine();
+                Console.Write("Introduzca el email: ");
+                email[x] = Console.ReadLine();
+                Console.Write("Introduzca la dirección: ");
+                direccion[x] = Console.ReadLine();
+
+
+
+            }
+
+        }
+
+
+
+        static void Eventos()
+        {
+            int opciones = 0;
+            
+            
+                Console.Clear();
+                Console.WriteLine("Introduzca el valor del menú en el que desea ingresar: " +
+              "" + "\n" + "[1] Agregar" + "\n" + "[2] Actualizar" + "\n" + "[3] Borrar" + "\n" + "[4] Buscar" + "\n" + "[5]atras");
+
+            string cadenaEvaluar = Console.ReadLine();
+            bool esLetra = cadenaEvaluar.All(char.IsLetter);
+            bool esNumero = cadenaEvaluar.All(char.IsNumber);
+            bool esNumer = false;
+            
+            try
+            {
+                opciones = Convert.ToInt32(cadenaEvaluar);
+            }
+            catch (Exception)
+            {
+                esNumer = true;
+
+            }
+            finally
+            {
+                if (esNumer == true || opciones > 5)
+                {
+                    Console.Clear();
+                    Eventos();
+
+                }
+                else
+                {
+                    switch (opciones)
+                    {
+                        case 1:
+
+                            x++;
+                            Console.Clear();
+                            Console.Write("Introduzca el nombre de la actividad: ");
+
+                            actividad[x] = Console.ReadLine();
+
+                            Console.Write("Introduzca la fecha: ");
+                            fecha[x] = Convert.ToString(Console.ReadLine());
+                            Console.Write("Introduzca la hora: ");
+                            hora[x] = Convert.ToString(Console.ReadLine());
+                            Console.WriteLine("introduce lugar");
+                            lugar[x] = Convert.ToString(Console.ReadLine());
+
+
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("actualizar evento");
+                            string busqueda2 = Console.ReadLine();
+
+                            Console.Clear();
+
+                            for (int x = 0; x < nombres.Length; x++)
+
+                                if (fecha[x] == busqueda2)
+                                {
+                                    Console.WriteLine("ACTUALIZA FECHA" + "\b\n" + fecha[x]);
+                                    actividad[x] = Convert.ToString(Console.ReadLine());
+                                    Console.WriteLine("NUEVO  FECHA" + "\b\n" + actividad[x]);
+                                    fecha[x] = Convert.ToString(Console.ReadLine());
+                                    Console.WriteLine("NUEVO NUMERO" + "\b\n" + hora[x]);
+                                    hora[x] = Convert.ToString(Console.ReadLine());
+                                    Console.WriteLine("NUEVA LUGAR" + "\b\n" + lugar[x]);
+                                    lugar[x] = Convert.ToString(Console.ReadLine());
+                                }
+
+                            Console.ReadKey();
+                            break;
+
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("BORRAR EVENTO");
+                            string busqueda3 = Console.ReadLine();
+
+                            Console.Clear();
+
+                            for (int x = 0; x < fecha.Length; x++)
+                            {
+                                if (fecha[x] == busqueda3)
+                                {
+                                    fecha[x] = null;
+                                }
+                            }
+                            Console.ReadKey();
+
+                            break;
+
+                        case 4:
+                            Console.Clear();
+                            Console.WriteLine("FECHA MARCADA");
+                            string busqueda = Console.ReadLine();
+
+                            Console.Clear();
+
+                            for (int x = 0; x < fecha.Length; x++)
+                            {
+                                if (fecha[x] == busqueda)
+                                {
+                                    Console.WriteLine(" proximo evento" + x + "\b\nfecha encotrada" + "\b\n" + fecha[x]);
+                                    Console.WriteLine("dia de evento" + "\b\n" + actividad[x]);
+                                    Console.WriteLine("tiempo de evento" + "\b\n" + hora[x]);
+                                    Console.WriteLine("lugar de evento" + "\b\n" + lugar[x]);
+                                }
+                            }
+                            Console.ReadKey();
+                            break;
+
+                        case 5:
+                            Console.Clear();
+                            Main(); break;
+                    }
+                    Console.ReadKey();
+                }
+            }
+            
+
+                
+
+            
+
+
+
+
+
+
+        }
+
+        static void UsToRd()
+        {
+             
+            int ulti = 0;
+            do
+            {
+
+                Console.Clear();
+                double usd = 51; double conv;
+                Console.Write("Ingrese una cantidad en dolares: ");
+                double num = Convert.ToDouble(Console.ReadLine());
+                conv = num * usd;
+                Console.Clear();
+                Console.WriteLine(num + " dolares equivale a " + conv + " pesos dominicanos");
+                Console.WriteLine("salir");
+                ulti = int.Parse(Console.ReadLine());
+                switch (ulti)
+                {
+                    case 1: Utilitarios(); break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            } while (ulti != 1); 
+        }
+        static void FtoC()
+        {
+            Console.Clear();
+            double fah;
+            Console.Write("Grado Celsius: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            fah = (num * 9 / 5) + 32;
+            Console.Clear();
+            Console.WriteLine("Grado Fahrenheit: " + fah);
+            Console.ReadKey();
+        }
+        static void CtoF()
+        {
+            Console.Clear();
+            double cel;
+            Console.Write("Grado Fahrenheit: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            cel = (num - 32) * 5 / 9;
+            Console.Clear();
+            Console.WriteLine("Grado Celsius: " + cel);
+            Console.ReadKey();
+        }
+        static void Salir()
+        {
+            Console.Clear();
+            Console.WriteLine("Saliendo...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void RdToUs()
+        {
+            Console.Clear();
+            double usd = 51; double conv;
+            Console.Write("Ingrese una cantidad en peso dominicano: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            conv = num / usd;
+            Console.Clear();
+            Console.WriteLine(num + " pesos dominicanos equivale a " + conv + " dolares");
+            Console.ReadKey();
+        }
+
+        static void Utilitarios()
+        {
+           
+                Console.Clear();
+                Console.WriteLine("Elija una opcion: \b\n [1] Conversor de RD$ a USD$ \b\n [2] Conversor de USD$ a RD$ \b\n [3] Conversor de Grados Fahrenheit a Celsius \b\n [4] Conversor de Grados Celsius a Fahrenheit \b\n [5] Salir");
+                //opcion = int.Parse(Console.ReadLine());
+            string cadenaEvaluar = Console.ReadLine();
+            bool esLetra = cadenaEvaluar.All(char.IsLetter);
+            bool esNumero = cadenaEvaluar.All(char.IsNumber);
+            bool esNumer = false;
+            int opcion = 0;
+            try
+            {
+                opcion = Convert.ToInt32(cadenaEvaluar);
+            }
+            catch (Exception)
+            {
+                esNumer = true;
+
+            }
+            finally
+            {
+                if (esNumer == 
+                    true || opcion > 5)
+                {
+                    Console.Clear();
+                    Main();
+
+                }
+                else
+                {
+
+
+                    switch (opcion)
+                    {
+
+                        case 1:
+                            RdToUs();
+                            break;
+                        case 2:
+                            UsToRd();
+                            break;
+                        case 3:
+                            FtoC();
+                            break;
+                        case 4:
+                            CtoF();
+                            break;
+                        case 5:
+                            Salir();
+                            Main();
+                            break;
+                            //default:
+
+
+
+                    }
+
+
+                }
+
+                //Console.ReadKey();
+                }
+            }
+            
+            
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
---
+++ pruedas de todo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pruedas_de_todo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DateTime fecha;
+           DateTime.Today.AddTicks(-fecha.AddTicks).year - 1;
+            Console.ReadKey();
+        }
+    }
+}
