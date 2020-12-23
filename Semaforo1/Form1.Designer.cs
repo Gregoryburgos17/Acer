587f8869b981105d78ec2a38de085782623478dc
Gregoryburgos17 <61199931+Gregoryburgos17@users.noreply.github.com>
2020-12-22 20:14:52 -0500

    Add files via upload

---
+++ Semaforo1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Semaforo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btencender = new System.Windows.Forms.Button();
+            this.btndetener = new System.Windows.Forms.Button();
+            this.btnapagar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btencender
+            // 
+            this.btencender.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btencender.Location = new System.Drawing.Point(371, 55);
+            this.btencender.Name = "btencender";
+            this.btencender.Size = new System.Drawing.Size(111, 42);
+            this.btencender.TabIndex = 0;
+            this.btencender.Text = "ENCENDER";
+            this.btencender.UseVisualStyleBackColor = true;
+            this.btencender.Click += new System.EventHandler(this.btencender_Click);
+            // 
+            // btndetener
+            // 
+            this.btndetener.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndetener.Location = new System.Drawing.Point(371, 116);
+            this.btndetener.Name = "btndetener";
+            this.btndetener.Size = new System.Drawing.Size(111, 37);
+            this.btndetener.TabIndex = 1;
+            this.btndetener.Text = "DETENER";
+            this.btndetener.UseVisualStyleBackColor = true;
+            this.btndetener.Click += new System.EventHandler(this.btndetener_Click);
+            // 
+            // btnapagar
+            // 
+            this.btnapagar.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnapagar.Location = new System.Drawing.Point(371, 180);
+            this.btnapagar.Name = "btnapagar";
+            this.btnapagar.Size = new System.Drawing.Size(111, 35);
+            this.btnapagar.TabIndex = 2;
+            this.btnapagar.Text = "APAGAR";
+            this.btnapagar.UseVisualStyleBackColor = true;
+            this.btnapagar.Click += new System.EventHandler(this.btnapagar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Semaforo1.Properties.Resources.apagado;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 286);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SEMAFORO ";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 351);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnapagar);
+            this.Controls.Add(this.btndetener);
+            this.Controls.Add(this.btencender);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btencender;
+        private System.Windows.Forms.Button btndetener;
+        private System.Windows.Forms.Button btnapagar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Semaforo1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semaforo1
+{
+    public partial class Form1 : Form
+    {
+        int caso = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Semaforo1.Properties.Resources.apagado;
+            btnapagar.Enabled = false;
+            btndetener.Enabled = false;
+        }
+
+        private void btencender_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btencender.Enabled = false;
+            btnapagar.Enabled = true;
+            btndetener.Enabled = true;
+            label1.Text = "SEMAFORO ON";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //crear un clase de sepappi donde resivira los datos de la app  y instancial 
+            switch (caso)
+            {
+                case 0:
+                    pictureBox1.Image = Semaforo1.Properties.Resources.verde;
+                    timer1.Interval = 10000;
+                    caso = 1;
+                    break;
+                case 1: pictureBox1.Image = Semaforo1.Properties.Resources.amarillo;
+                    timer1.Interval = 5000;
+                    caso = 2;
+                    break;
+                case 2: pictureBox1.Image = Semaforo1.Properties.Resources.rojo;
+                    timer1.Interval = 5000;
+                    caso = 0;
+                    break;
+            }
+        }
+
+        private void btndetener_Click(object sender, EventArgs e)
+        {
+            if (btndetener.Text == "DETENER")
+            {
+                timer1.Enabled = false;
+                btndetener.Text = "REANUDAR";
+                label1.Text = "SEMAFORO DETENIDO";
+            }
+            else
+            if (btndetener.Text == "REANUDAR")
+            {
+                timer1.Enabled = true;
+                btndetener.Text = "DETENER";
+                label1.Text = "SEMAFORO ON";
+            }
+                
+            
+        }
+
+        private void btnapagar_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btencender.Enabled = true;
+            btnapagar.Enabled = false;
+            btndetener.Enabled = false;
+            pictureBox1.Image = Semaforo1.Properties.Resources.apagado;
+            label1.Text = "SEMAFORO OFF";
+        }
+    }
+}
