47696b20007f22d0a0a24103bdeaad02f810c065
Gregoryburgos17 <61199931+Gregoryburgos17@users.noreply.github.com>
2020-12-22 20:18:43 -0500

    Add files via upload

---
+++ IMPAR/Program.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMPAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            Console.Clear();
+
+            Console.WriteLine("Elija en programa a ejecutar:\b\n 1.-  En qué posición del vector está el mayor número leído. \b\n 2.- En qué posiciones se encuentran los números que comienzan en 4. \b\n 3.- Determinar cuántos números negativos hay\b\n 4.-  En qué posiciones están los números positivos. \b\n 5.- Cuál es el número menor.  \b\n 6.- Determinar si la semisuma entre el valor mayor y el valor menor es un número par \b\n 7.- Determinar si la semisuma entre el valor mayor y el valor menor es un número primo\b\n 8.- Cuántos números de los almacenados en dicho vector comienzan por 34. ");
+            menu = int.Parse(Console.ReadLine());
+            switch (menu)
+
+            {
+                case 1:
+                    Console.Clear();
+
+                    Console.WriteLine("Leer 10 enteros, almacenarlos en un vector y determinar en qué posición del vector está el mayor número leído");
+                    int[] numero = new int[10];
+                    for (int x = 0; x < numero.Length; x++)
+                    {
+                        numero[x] = 0;
+                        Console.WriteLine("Digite un numero en la posicion{0}", x);
+                        int.TryParse(Console.ReadLine(), out numero[x]);
+
+                    }
+                    int mayor = 0;
+                    int posicion = 0;
+                    for (int x = 0; x < numero.Length; x++)
+                    {
+                        if (numero[x] > mayor)
+                        {
+                            mayor = numero[x];
+                            posicion = x;
+
+
+                        }
+                    }
+                    Console.WriteLine("El mayor es{0} y esta en la posicion{1}", mayor, posicion);
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("Leer 10 números enteros, almacenarlos en un vector y determinar en qué posiciones se encuentran los números que comienzan en 4");
+                    int[] num = new int[10];
+                    for (int ll = 0; ll < num.Length; ll++)
+                    {
+                        num[ll] = 0;
+                        Console.WriteLine("Digite un numero", ll);
+                        int.TryParse(Console.ReadLine(), out num[ll]);
+
+
+
+
+                    }
+                    for (int ll = 0; ll < num.Length; ll++)
+                    {
+                        if (num[ll] / 10 == 4 || num[ll] / 1 == 4)
+                        {
+                            string t = num[ll].ToString();
+
+                            Console.WriteLine("El {0}comienza en 4", t);
+                        }
+
+
+                    }
+                    Console.Read();
+
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("Leer 10 números enteros, almacenarlos en un vector y determinar cuántos números negativos hay");
+                    int[] nu = new int[10];
+                    for (int a = 0; a < nu.Length; a++)
+                    {
+                        nu[a] = 0;
+                        Console.WriteLine("Digite un numero", a);
+                        int.TryParse(Console.ReadLine(), out nu[a]);
+
+
+
+
+                    }
+                    for (int a = 0; a < nu.Length; a++)
+                    {
+                        if (nu[a] < 0)
+                        {
+                            string t = nu[a].ToString();
+
+                            Console.WriteLine("El {0} es negativo", t);
+                        }
+
+
+                    }
+                    Console.Read();
+
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Leer 10 números enteros, almacenarlos en un vector y determinar en qué posiciones están los números positivos");
+                    int[] n = new int[10];
+                    for (int m = 0; m < n.Length; m++)
+                    {
+                        n[m] = 0;
+                        Console.WriteLine("Digite un numero", m);
+                        int.TryParse(Console.ReadLine(), out n[m]);
+
+
+
+
+                    }
+                    for (int m = 0; m < n.Length; m++)
+                    {
+                        if (n[m] > 0)
+                        {
+                            string t = n[m].ToString();
+
+                            Console.WriteLine("El {0}numero es positivo", t);
+                        }
+
+
+
+                    }
+                    Console.ReadKey();
+                    break;
+                case 5:
+                    Console.Clear();
+                    Console.WriteLine("Leer 10 números enteros, almacenarlos en un vector y determinar cuál es el número menor");
+                    int[] nm = new int[10];
+                    for (int f = 0; f < nm.Length; f++)
+                    {
+                        nm[f] = 0;
+                        Console.WriteLine("Digite un numero", f);
+                        int.TryParse(Console.ReadLine(), out nm[f]);
+
+
+
+
+                    }
+                    for (int f = 0; f < nm.Length; f++)
+                    {
+
+                        if (nm[f] > 0)
+                        {
+                            string t = nm[f].ToString();
+
+                            Console.WriteLine("El {0}numero es positivo", t);
+                        }
+
+
+
+                    }
+                    Console.ReadKey();
+                    break;
+                case 6:
+                    Console.Clear();
+
+                    Console.WriteLine("Leer 10 números enteros, almacenarlos en un vector y determinar si la semisuma entre el valor mayor y el valor menor es un número par");
+                    int i;
+                    int[] n1 = new int[10];
+                    int mayol = -1, indice = 0, semisuma = 0;
+
+
+                    for (i = 0; i < n1.Length; i++)
+                    {
+                        Console.WriteLine("Digite un numero{0}", i);
+                        int.TryParse(Console.ReadLine(), out n1[i]);
+                    }
+                    for (i = 0; i < n1.Length; i++)
+                    {
+                        Console.WriteLine(i + "  -  " + n1[i]);
+
+
+
+                    }
+                    for (i = 0; i < n1.Length; i++)
+                    {
+                        if (n1[i] > mayol)
+                        {
+                            mayol = n1[i];
+
+                            indice = i;
+                        }
+
+
+
+                    }
+                    Console.WriteLine("El mayor{0} y su posicion{1}", mayol, indice);
+                    int j = 0;
+                    int indice1 = 0;
+                    int menor = n1[j];
+                    for (j = 0; j < n1.Length; j++)
+                    {
+                        if (n1[j] < menor)
+                        {
+                            menor = n1[j];
+                            indice1 = j;
+
+                        }
+                    }
+                    Console.WriteLine("El menor{0} y su posicion{1}", menor, indice1);
+                    semisuma = menor + mayol;
+                    Console.WriteLine("La semisuma es:" + semisuma);
+                    if (semisuma % 2 == 0)
+                    {
+                        Console.WriteLine("La semisuma es un nuemero par");
+                    }
+                    else
+                    {
+                        Console.WriteLine("La semisuma no es un nuemero par");
+                    }
+
+
+                    Console.ReadKey();
+
+
+                    break;
+                case 7:
+                    Console.Clear();
+                    Console.WriteLine("Leer 10 números enteros, almacenarlos en un vector y determinar si la semisuma entre el valor mayor y el valor menor es un número primo");
+                    int l;
+                    int[] n11 = new int[10];
+                    int mayo = -1, indic = 0, semisum = 0;
+
+
+                    for (l = 0; l < n11.Length; l++)
+                    {
+                        Console.WriteLine("Digite un numero{0}", l);
+                        int.TryParse(Console.ReadLine(), out n11[l]);
+                    }
+                    for (l = 0; l < n11.Length; l++)
+                    {
+                        Console.WriteLine(l + "  -  " + n11[l]);
+
+
+
+                    }
+                    for (l = 0; l < n11.Length; l++)
+                    {
+                        if (n11[l] > mayo)
+                        {
+                            mayo = n11[l];
+
+                            indic = l;
+                        }
+
+
+
+                    }
+                    Console.WriteLine("El mayor{0} y su posicion{1}", mayo, indic);
+                    int k = 0;
+                    int indice2 = 0;
+                    int meno = n11[k];
+                    for (k = 0; k < n11.Length; k++)
+                    {
+                        if (n11[k] < meno)
+                        {
+                            meno = n11[k];
+                            indice2 = k;
+
+                        }
+                    }
+                    Console.WriteLine("El menor{0} y su posicion{1}", meno, indice2);
+                    semisuma = meno + mayo;
+                    Console.WriteLine("La semisuma es:" + semisum);
+                    if (semisum % 1 == 0)
+                    {
+                        Console.WriteLine("La semisuma es un nuemero par");
+                    }
+                    else
+                    {
+                        Console.WriteLine("La semisuma no es un nuemero par");
+                    }
+
+
+                    Console.ReadKey();
+
+                    break;
+
+                case 8:
+                    Console.Clear();
+                    Console.Write("Leer 10 números enteros, almacenarlos en un vector y determinar cuántos números de los almacenados en dicho vector comienzan por 34");
+                    int[] n2 = new int[10];
+                    for (int lll = 0; lll < n2.Length; lll++)
+                    {
+                        n2[lll] = 0;
+                        Console.WriteLine("Digite un numero", lll);
+                        int.TryParse(Console.ReadLine(), out n2[lll]);
+
+
+
+
+                    }
+                    for (int lll = 0; lll < n2.Length; lll++)
+                    {
+                        if (n2[lll] / 10 == 34 || n2[lll] / 1 == 34)
+                        {
+                            string t = n2[lll].ToString();
+
+                            Console.WriteLine("El {0}comienza en 34", t);
+                        }
+
+
+                    }
+                    Console.Read();
+                    break;
+            }
+        }
+    }
+}
---
+++ imparejercio/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imparejercio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 0;
+            Console.WriteLine("\b\nOpcion: ");
+            op = int.Parse(Console.ReadLine());
+            Console.Clear();
+            switch (op)
+            {
+                case 9:
+                    int[] arr2 = new int[10];
+                    int m2 = 0;
+                    Console.WriteLine("Ingrese 10 numeros enteros:");
+                    for (int i = 0; i < arr2.Length; i++)
+                    {
+                        Console.WriteLine("Numero " + (i + 1) + ":");
+                        arr2[i] = int.Parse(Console.ReadLine());
+                    }
+                    for (int i = 0; i < arr2.Length; i++)
+                    {
+                        m2 += arr2[i];
+                    }
+                    m2 = m2 / arr2.Length;
+                    Console.Clear();
+                    Console.WriteLine("El con mas de 3 digitos es : " + m2);
+                    Console.ReadKey();
+
+
+                    break;
+                case 11:
+                    int[] arr = new int[10];
+                    int m = 0;
+                    int p = 0;
+                    Console.WriteLine("Ingrese 10 numeros enteros:");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        Console.WriteLine("Numero " + (i + 1) + ":");
+                        arr[i] = int.Parse(Console.ReadLine());
+                    }
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        m += arr[i];
+                    }
+                    m = m / arr.Length;
+                    Console.Clear();
+                    Console.WriteLine("El promedio entero es: " + m);
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (arr[i] == m)
+                        {
+                            p++;
+                        }
+                    }
+                    break;
+                case 13:
+                    string[] arr3 = new string[10];
+                    int[] arr4 = new int[10];
+                    int m1= 0;
+                    Console.WriteLine("Ingrese 10 numeros enteros:");
+                    for (int i = 0; i < arr3.Length; i++)
+                    {
+                        Console.WriteLine("Numero " + (i + 1) + ":");
+                        arr3[i] = Console.ReadLine();
+                        if (arr3[i].Length >= 3)
+                        {
+                            arr4[m1] = i;
+                            m1++;
+                        }
+                    }
+                    Array.Resize(ref arr4, m1);
+                    Console.Clear();
+                    Console.WriteLine("Posiciones de Numeros con mas de 3 digitos leidos: ");
+                    for (int i = 0; i < arr4.Length; i++)
+                    {
+                        Console.WriteLine(arr4[i]);
+                    }
+                    Console.ReadKey();
+                    break;
+                case 15:
+                    int[] arr5 = new int[10];
+                    int[] arr6 = new int[10];
+                    int m3 = 0;
+                    Console.WriteLine("Ingrese 10 numeros enteros:");
+                    for (int i = 0; i < arr5.Length; i++)
+                    {
+                        Console.WriteLine("Numero " + (i + 1) + ":");
+                        arr5[i] = int.Parse(Console.ReadLine());
+                        if (arr5[i] % 3 == 0)
+                        {
+                            arr6[m3] = arr5[i];
+                            m3++;
+                        }
+                    }
+                    Array.Resize(ref arr6, m3);
+                    Console.Clear();
+                    Console.WriteLine("Numeros multiplos de 3: ");
+                    for (int i = 0; i < arr6.Length; i++)
+                    {
+                        Console.WriteLine(arr6[i]);
+                    }
+                    Console.ReadKey();
+                    break;
+                case 17:
+                    int[] arr7 = new int[10];
+                    int[] arr8 = new int[10];
+                    int m4 = 0;
+                    Console.WriteLine("Ingrese 10 numeros enteros:");
+                    for (int i = 0; i < arr7.Length; i++)
+                    {
+                        Console.WriteLine("Numero " + (i + 1) + ":");
+                        arr7[i] = int.Parse(Console.ReadLine());
+                        if (arr7[i] > 0)
+                        {
+                            arr8[m4] = i;
+                            m4++;
+                        }
+                    }
+                    Array.Resize(ref arr8, m4);
+                    Console.Clear();
+                    Console.WriteLine("Posiciones de numeros positivos: ");
+                    for (int i = 0; i < arr8.Length; i++)
+                    {
+                        Console.WriteLine(arr8[i]);
+                    }
+                    Console.ReadKey();
+            
+
+                       break;
+                case 19:
+                    int[] arr9 = new int[10];
+                    int pos = 0; int contp = 0; int n= 0;
+
+                    Console.WriteLine("Ingrese 10 numeros enteros:");
+                    for (int i = 0; i < arr9.Length; i++)
+                    {
+                        Console.WriteLine("Numero " + (i + 1) + ":");
+                        arr9[i] = int.Parse(Console.ReadLine());
+                        for (int u = 1; u < (arr9[i] + 1); u++)
+                        {
+                            if (arr9[i] % u == 0)
+                            {
+                                if (arr9[i] > arr9[pos])
+                                {
+                                    pos = i;
+                                }
+                                contp++;
+                            }
+                        }
+                        if (contp == 2)
+                        {
+                            if (arr9[i] < arr9[n])
+                            {
+                                n = i;
+                            }
+                        }
+                        contp = 0;
+                    }
+                    Console.Clear();
+                    Console.WriteLine("El numero primo menor esta en la posicion: " + n);
+                    Console.ReadKey();
+                    break;
+
+
+
+
+                    //17. Leer 10 números enteros, almacenarlos en un vector y determinar cuántos números negativos hay. 
+                    //19. Leer 10 números enteros, almacenarlos en un vector y determinar cuál es el número menor. 
+
+            }
+        }
+    }
+                  
+}
